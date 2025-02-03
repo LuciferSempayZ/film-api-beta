1704f9df38495c680338b551984119cb406f4397
LuciferSempayZ <grigegoron@gmail.com>
2025-02-04 00:43:42 +0700

    Удалил из сидеров рейтинг (забыл)

--- database/seeders/RatingsSeeder.php
+++ database/seeders/RatingsSeeder.php
@@ -11,8 +11,8 @@ class RatingsSeeder extends Seeder
     public function run()
     {
         DB::table('rating')->insert([
-            ['movies_id' => 1, 'review_text' => 'Amazing movie!', 'rating' => 9.5, 'users_id' => 1],
-            ['movies_id' => 2, 'review_text' => 'Loved it!', 'rating' => 8.7, 'users_id' => 2],
+            ['movies_id' => 1, 'review_text' => 'Amazing movie!', 'users_id' => 1],
+            ['movies_id' => 2, 'review_text' => 'Loved it!', 'users_id' => 2],
         ]);
     }
 }
